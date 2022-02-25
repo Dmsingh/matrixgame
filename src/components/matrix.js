b156716bd581b6226f72c8275f2175b01b62a231
Dmsingh <dm.singh723dm@gmail.com>
2022-02-25 13:47:31 +0530

    fixing alerts and design

--- src/components/matrix.js
+++ src/components/matrix.js
@@ -10,143 +10,149 @@ function Matrix() {
     
     
     const changehandler =(e)=>{
-        if(click===0 || click===2){
-            alert("box",box1)
-            try{
+        if(counter<=16)
+        {
+            if(click===0 || click===2){
+        
+                try{
+                    console.log(e.target.id)
+                    document.getElementById(`div${e.target.id}`).style.backgroundColor='red'
+                    document.getElementById(`${e.target.id}`).style.color='white'
+                    document.getElementById(`${e.target.id}`).innerHTML=`#box${counter}`
+                }catch(e){
+                    
+                    console.log(e,'try')
+                }
+                
+    
+              
+                
+        
+                
+                if(box1)
+                {
+                    
+                    try{
+                        document.getElementById(`div${box1}`).style.backgroundColor='blue'
+    
+                    }
+                    catch(e){
+                        
+                        console.log(e,'try')
+                    }
+    
+                }
+                
+                
+                setbox1(`${e.target.id}`)
+                
+                setclick(1)
+                const count=counter+1
+            
+                setcounter(count)
+    
+            }
+            if(click===1){
+                
                 document.getElementById(`div${e.target.id}`).style.backgroundColor='red'
                 document.getElementById(`${e.target.id}`).style.color='white'
-                document.getElementById(`${e.target.id}`).innerHTML=`${counter}`
-            }catch(e){
-                
-                console.log(e,'try')
-            }
-            
-
-          
+                document.getElementById(`${e.target.id}`).innerHTML=`#box${counter}`
             
     
-            
-            if(box1)
-            {
-                
-                try{
-                    document.getElementById(`div${box1}`).style.backgroundColor='blue'
-
-                }
-                catch(e){
+                if(box2 )
+                {
                     
-                    console.log(e,'try')
+                    try{
+                        document.getElementById(`div${box2}`).style.backgroundColor='blue'
+    
+                    }
+                    catch(e){
+                        
+                        console.log(e,'try')
+                    }
+    
                 }
-
-            }
-            
-            
-            setbox1(`${e.target.id}`)
-            
-            setclick(2)
-            const count=counter+1
-        
-            setcounter(count)
-
-        }
-        if(click===1){
-            document.getElementById(`div${e.target.id}`).style.backgroundColor='red'
-            document.getElementById(`${e.target.id}`).style.color='white'
-            document.getElementById(`${e.target.id}`).innerHTML=`${counter}`
-        
-
-            if(box2 )
-            {
                 
-                try{
-                    document.getElementById(`div${box1}`).style.backgroundColor='blue'
-
-                }
-                catch(e){
-                    
-                    console.log(e,'try')
-                }
-
+                setbox2(`${e.target.id}`)
+                setclick(2)
+                setcounter(counter+1)
+    
             }
-            
-            setbox2(`${e.target.id}`)
-            setclick(1)
-            setcounter(counter+1)
-
         }
+      
     }
   return (
     <div id="container">
         <div style={{display: 'flex', }}>
-        <div onClick={(e)=>{changehandler(e)}} id="div1" class="box">
-    <p id="1" class="contents">1</p>
+        <div onClick={(e)=>{changehandler(e)}} id="div1" className="box">
+    <p id="1" className="contents">1</p>
   </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div2" class="box">
+  <div onClick={(e)=>{changehandler(e)}} id="div2" className="box">
 
-    <p id="2" class="contents">2</p>
+    <p id="2" className="contents">2</p>
   </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div3" class="box">
+  <div onClick={(e)=>{changehandler(e)}} id="div3" className="box">
 
-    <p id="3" class="contents">3</p>
+    <p id="3" className="contents">3</p>
   </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div4" class="box">
+  <div onClick={(e)=>{changehandler(e)}} id="div4" className="box">
 
-    <p id="4" class="contents">4</p>
+    <p id="4" className="contents">4</p>
   </div>
         </div>
   
 
         <div style={{display: 'flex', }}>
-        <div onClick={(e)=>{changehandler(e)}} id="div4" class="box">
-    <p id="5" class="contents">5</p>
+        <div onClick={(e)=>{changehandler(e)}} id="div5" className="box">
+    <p id="5" className="contents">5</p>
   </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div6" class="box">
+  <div onClick={(e)=>{changehandler(e)}} id="div6" className="box">
 
-    <p id="6" class="contents">6</p>
+    <p id="6" className="contents">6</p>
   </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div7" class="box">
+  <div onClick={(e)=>{changehandler(e)}} id="div7" className="box">
 
-    <p id="7" class="contents">7</p>
+    <p id="7" className="contents">7</p>
   </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div8" class="box">
+  <div onClick={(e)=>{changehandler(e)}} id="div8" className="box">
 
-    <p id="8" class="contents">8</p>
-  </div>
-        </div>
-
-        <div style={{display: 'flex', }}>
-  <div onClick={(e)=>{changehandler(e)}} id="div9" class="box">
-    <p id="9" class="contents">9</p>
-  </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div10" class="box">
-
-    <p id="10" class="contents">10</p>
-  </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div11" class="box">
-
-    <p id="11" class="contents">11</p>
-  </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div12" class="box">
-
-    <p id="12" class="contents">12</p>
+    <p id="8" className="contents">8</p>
   </div>
         </div>
 
         <div style={{display: 'flex', }}>
-<div onClick={(e)=>{changehandler(e)}} id="div13" class="box">
-    <p id="13" class="contents">13</p>
+  <div onClick={(e)=>{changehandler(e)}} id="div9" className="box">
+    <p id="9" className="contents">9</p>
   </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div14" class="box">
+  <div onClick={(e)=>{changehandler(e)}} id="div10" className="box">
 
-    <p id="14" class="contents">14</p>
+    <p id="10" className="contents">10</p>
   </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div15" class="box">
+  <div onClick={(e)=>{changehandler(e)}} id="div11" className="box">
 
-    <p  id="15" class="contents">15</p>
+    <p id="11" className="contents">11</p>
   </div>
-  <div onClick={(e)=>{changehandler(e)}} id="div16" class="box">
+  <div onClick={(e)=>{changehandler(e)}} id="div12" className="box">
 
-    <p id="16" class="contents">16</p>
+    <p id="12" className="contents">12</p>
+  </div>
+        </div>
+
+        <div style={{display: 'flex', }}>
+<div onClick={(e)=>{changehandler(e)}} id="div13" className="box">
+    <p id="13" className="contents">13</p>
+  </div>
+  <div onClick={(e)=>{changehandler(e)}} id="div14" className="box">
+
+    <p id="14" className="contents">14</p>
+  </div>
+  <div onClick={(e)=>{changehandler(e)}} id="div15" className="box">
+
+    <p  id="15" className="contents">15</p>
+  </div>
+  <div onClick={(e)=>{changehandler(e)}} id="div16" className="box">
+
+    <p id="16" className="contents">16</p>
   </div>
         </div>
 </div>
